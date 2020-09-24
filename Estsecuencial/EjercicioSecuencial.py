6b9c3945fa4a777f4683c61f0756bff8959d4d4b
repl.it user <replituser@example.com>
2020-09-24 13:39:52 +0000

    Ejercicio secuencial

---
+++ Estsecuencial/EjercicioSecuencial.py
@@ -0,0 +1,12 @@
+def CalcularpromedioNota():
+  #Datos entrada
+  N1=int(input("Ingrese la Nota 1: "))
+  N2=int(input("Ingrese la Nota 2: "))
+  N3=int(input("Ingrese la Nota 3: "))
+  N4=int(input("Ingrese la Nota 4: "))
+  #Proceso
+  PC=(N1+N2+N3+N4)/4
+  #Datos de salida
+  print ("Su promedio es:",PC)
+
+CalcularpromedioNota()
